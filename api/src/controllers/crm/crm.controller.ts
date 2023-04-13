9abcc553b3fd8ccf473c5e86b628a179374f6715
Andrey <a.alymov@pandarium.pro>
2023-04-13 12:33:29 +0700

    + Мелочи

--- api/src/controllers/crm/crm.controller.ts
+++ api/src/controllers/crm/crm.controller.ts
@@ -2,7 +2,7 @@ import {Controller, Get, Redirect} from '@nestjs/common';
 
 @Controller('crm')
 export class CrmController {
-    @Redirect("http://25.51.146.92:4200", 301)
+    @Redirect("http://192.168.31.22:4200", 301)
     @Get()
     goToCRM() {}
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -5,29 +5,10 @@ import { join } from 'path';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 
-const PORT = 443;
+const PORT = 80;
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-		cors: true
-	});
-	app.use((req, res, next) => {
-		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Headers', '*');
-		res.header(
-			'Access-Control-Allow-Headers',
-			'Origin, Content-Type, Accept, X-Requested-With, app_token, Authorization',
-		);
-		res.header('Content-Security-Policy', "default-src 'self'");
-		if (req.method === 'OPTIONS') {
-			res.header(
-				'Access-Control-Allow-Methods',
-				'PUT, PATCH, POST, DELETE, GET',
-			);
-			return res.status(200).json({});
-		}
-		next();
-	});
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	const assets = join(__dirname, '..', 'public');
 	app.useStaticAssets(assets);
 	app.setGlobalPrefix('api');
